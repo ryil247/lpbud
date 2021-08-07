4645ae18562e70f2c351e3d5f9cbe50e24b95c44
adlerhurst <silvan.reusser@gmail.com>
2021-08-07 14:50:12 +0200

    init gui and backend

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/ryil247/lpbud/backend
+
+go 1.16
---
+++ gui/android/app/src/main/kotlin/com/example/lpbud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lpbud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
